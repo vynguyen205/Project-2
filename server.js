43557f5a7027ea649ff2d979afe13f00d76567a5
Vy Nguyen <vynguyen205@Vys-MacBook-Air.local>
2022-06-25 10:41:54 -0700

    added dev dependencies

--- server.js
+++ server.js
@@ -4,4 +4,8 @@ const path = require('path');
 const session = require('express-session');
 
 const app = express();
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 const PORT = process.env.PORT || 3001;
