7ced748aaff8170b6b9e67548825ca1321ea535b
Vy Nguyen <vynguyen205@Vys-MacBook-Air.local>
2022-06-25 11:09:30 -0700

    added more express method to server.js

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
+//required libraries
 const express = require('express');
 const routes = require('./controllers');
 const path = require('path');
 const session = require('express-session');
+// const exphbs = require('express-handlebars');
+
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+//bring in handlebars modules
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
+//Define template engine to use
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-const PORT = process.env.PORT || 3001;
+//express built-in middleware to serve static files
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+//defined routes for the app
+app.use(routes);
+
+// connect-session-sequelize middleware
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+  });
+  
