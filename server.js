126a78bc1734b062d3dc8bdbb9b1f4bb12f3245c
Vy Nguyen <vynguyen205@Vys-MacBook-Air.local>
2022-06-26 18:19:33 -0700

    added routes folder

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const routes = require('./controllers');
 const path = require('path');
 const session = require('express-session');
+
 // const exphbs = require('express-handlebars');
 
 
