a2f8b2a23b7bec4cdb1789142a6e72e7654450b5
dennismacc <dennis.maccarthy@gmail.com>
2022-06-28 15:18:24 -0700

    pull

---
+++ controllers/api/wordsRoutes.js
@@ -0,0 +1,37 @@
+const router = require('express').Router();
+const Words = require('../../models/words');
+
+// Get route for retrieving words
+router.get('/', async (req, res) => {
+  const wordsData = await Words.findAll();
+  console.log('wordsData', wordsData);
+  return res.json(wordsData);
+});
+
+router.post('/', async (req, res) => {
+  const wordsData = await Words.create(req.params.words);
+  return res.json(wordsData);
+});
+
+router.get('/:id', async (req, res) => {
+  const wordsData = await Words.findByPk(req.params.id);
+  return res.json(wordsData);
+});
+
+router.get('/:word', async (req, res) => {
+  const wordsData = await Words.findByPk(req.params.word);
+  return res.json(wordsData);
+});
+
+router.get('/:points', async (req, res) => {
+  const wordsData = await Words.findByPk(req.params.points);
+  return res.json(wordsData);
+});
+
+router.post('/', async (req, res) => {
+  const wordsData = await Words.create(req.body);
+  return res.json(wordsData);
+});
+
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api/apiRoutes');
 const homeRoutes = require('./homeRoutes');
+const wordsRoutes = require('./api/wordsRoutes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/api', wordsRoutes); 
 
 module.exports = router;
