0608aba502b4e550fd9f19e74cd7215aba25eeff
dennismacc <dennis.maccarthy@gmail.com>
2022-06-30 19:41:31 -0700

    finalize userRoute

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,20 +1,53 @@
 const router = require('express').Router();
-const { Router } = require('express');
 const User = require('../../models/users');
 
 // Get route for retrieving users
 router.get('/', async (req, res) => {
-    const userData = await User.findByPk;
-    console.log('userData', userData);
-    return res.json(userData);
+  const userData = await User.findByPk;
+  console.log('userData', userData);
+  return res.json(userData);
 });
 
 router.post('/', async (req, res) => {
-    const userData = await User.create(req.params.user);
-    return res.json(userData);
+  const userData = await User.create(req.params.user);
+  return res.json(userData);
 });
 
+//Get data from users model (id, socket_id, username, highscore, avatar, room_id)
+router.get('/:id', async (req, res) => {
+  const userData = await User.findByPk(req.params.id);
+  return res.json(userData);
+});
 
+router.get('/:socket_id', async (req, res) => {
+  const userData = await User.findByPk(req.params.socket_id);
+  return res.json(userData);
+});
 
+router.get('/:username', async (req, res) => {
+  const userData = await User.findByPk(req.params.username);
+  return res.json(userData);
+});
 
+router.get('/:highscore', async (req, res) => {
+  const userData = await User.findByPk(req.params.highscore);
+  return res.json(userData);
+});
 
+router.get('/:avatar', async (req, res) => {
+  const userData = await User.findByPk(req.params.avatar);
+  return res.json(userData);
+});
+
+router.get('/:room_id', async (req, res) => {
+  const userData = await User.findByPk(req.params.room_id);
+  return res.json(userData);
+});
+
+// Create user from user data that was pulled from users model 
+router.post('/', async (req, res) => {
+  const userData = await User.create(req.body);
+  return res.json(userData);
+});
+
+module.exports = router;
