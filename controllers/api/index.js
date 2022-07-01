21a613ceb93fdd9baac074250a58354a6db4b7b8
dennismacc <dennis.maccarthy@gmail.com>
2022-06-30 19:29:11 -0700

    working on userRoute

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,12 +1,11 @@
 const router = require('express').Router();
 const wordsRoute = require('./wordsRoute');
-// const userRoutes = require('./userRoutes');
-// const { route } = require('./wordsRoute');
+const userRoutes = require('./userRoutes');
 const gameRoomRoute = require('../homeRoutes');
 const lobbyRoute = require('../homeRoutes');
 
 router.use('/words', wordsRoute);
-// router.use('/users', userRoutes);
+router.use('/users', userRoutes);
 router.use('/gameRoom', gameRoomRoute);
 router.use('/lobby',lobbyRoute);
 
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,3 +1,20 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { Router } = require('express');
+const User = require('../../models/users');
+
+// Get route for retrieving users
+router.get('/', async (req, res) => {
+    const userData = await User.findByPk;
+    console.log('userData', userData);
+    return res.json(userData);
+});
+
+router.post('/', async (req, res) => {
+    const userData = await User.create(req.params.user);
+    return res.json(userData);
+});
+
+
+
+
 
