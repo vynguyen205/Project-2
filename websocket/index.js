31903903e064a952888c0a101b6e41ff112cfba5
Marina H <mxhuisken@gmail.com>
2022-07-03 12:41:56 -0700

    fixed bugs

--- websocket/index.js
+++ websocket/index.js
@@ -12,12 +12,12 @@ const createWSEvents = async io => {
         io.on('connection', socket => {
             console.log(chalk.green(`Client Connected`, socket.id));
         
-            socket.on("createRoom", ({room_name}, user) => {
-                const newUser = {
-                    user_id: user.id,
-                    socket_id: socket.id,
-                }
-                //check to see if someone already made a room with the same id
+            // socket.on("createRoom", ({room_name}, user) => {
+            //     const newUser = {
+            //         user_id: user.id,
+            //         socket_id: socket.id,
+                })
+            //     //check to see if someone already made a room with the same id
                 if(room_name === room_name) {
                     console.log(chalk.red("Room already exists"));
                     socket.emit("Room Exists: ", room_name);
@@ -25,7 +25,6 @@ const createWSEvents = async io => {
                 else {
                     console.log(chalk.yellow("Creating Room: ", room_name));
                 }
-            })
         
             socket.on("Disconnect", () => {
                 //removing the user from the array of users connected to the server
@@ -33,7 +32,6 @@ const createWSEvents = async io => {
                 //broadcasting the user to all other users. letting them know that a user has left and there's only that many users left
                 io.emit("New User", users);
             });
-    });
 
     } catch (err) {
     console.log(chalk.redBright(`🚨🚨🚨 SOMETHING WENT WRONG 🚨🚨🚨`, JSON.stringify(err)));
