2225098f2acbd930e47511a0b888758d0ee2de91
dennismacc <dennis.maccarthy@gmail.com>
2022-07-03 16:43:17 -0700

    working on logic for creating a new user

--- public/js/frontend.js
+++ public/js/frontend.js
@@ -11,6 +11,7 @@ const joinRoomPass = document.querySelector('#join-room-password');
 //adds a new user to the server
 let users = [];
 
+// Confirm that user is connected to the server
 const socket = io();
 socket.on ('connection', () => {
   console.log('⛓ Connected to server');
@@ -18,6 +19,7 @@ socket.on ('connection', () => {
 });
 socket.emit('💃🏻 Join Server', socket.id);
 
+// get data from server to create a new room 
 const createRoom = async () => {
   const room_name = createRoomName.value.trim();
   const roomPassword = createRoomPassword.value.trim();
@@ -77,3 +79,93 @@ const joinRoomFunction = async () => {
 createRoomBtn.addEventListener('click', createRoom);
 joinRoomBtn.addEventListener('click', joinRoomFunction);
 
+
+// NEW USER MODAL WINDOW
+
+  // Original JavaScript code by Chirp Internet: chirpinternet.eu
+  // Please acknowledge use of this code by including this header.
+var checkForm = function(e)
+  {
+    var form = (e.target) ? e.target : e.srcElement;
+    if(form.name.value == "") {
+      alert("Please enter your Name");
+      form.name.focus();
+      e.preventDefault ? e.preventDefault() : e.returnValue = false;
+      return;
+    }
+  };
+
+  var modal_init = function() {
+
+    var modalWrapper = document.getElementById("modal_wrapper");
+    var modalWindow  = document.getElementById("modal_window");
+
+    var openModal = function(e)
+    {
+      modalWrapper.className = "overlay";
+      var overflow = modalWindow.offsetHeight - document.documentElement.clientHeight;
+      if(overflow > 0) {
+        modalWindow.style.maxHeight = (parseInt(window.getComputedStyle(modalWindow).height) - overflow) + "px";
+      }
+      modalWindow.style.marginTop = (-modalWindow.offsetHeight)/2 + "px";
+      modalWindow.style.marginLeft = (-modalWindow.offsetWidth)/2 + "px";
+      e.preventDefault ? e.preventDefault() : e.returnValue = false;
+    };
+
+    var closeModal = function(e)
+    {
+      modalWrapper.className = "";
+      e.preventDefault ? e.preventDefault() : e.returnValue = false;
+    };
+
+    var clickHandler = function(e) {
+      if(!e.target) e.target = e.srcElement;
+      if(e.target.tagName == "DIV") {
+        if(e.target.id != "modal_window") closeModal(e);
+      }
+    };
+
+    var keyHandler = function(e) {
+      if(e.keyCode == 27) closeModal(e);
+    };
+
+    if(document.addEventListener) {
+      document.getElementById("modal_open").addEventListener("click", openModal, false);
+      document.getElementById("modal_close").addEventListener("click", closeModal, false);
+      document.addEventListener("click", clickHandler, false);
+      document.addEventListener("keydown", keyHandler, false);
+    } else {
+      document.getElementById("modal_open").attachEvent("onclick", openModal);
+      document.getElementById("modal_close").attachEvent("onclick", closeModal);
+      document.attachEvent("onclick", clickHandler);
+      document.attachEvent("onkeydown", keyHandler);
+    }
+
+    if(document.addEventListener) {
+      document.getElementById("modal_feedback").addEventListener("submit", checkForm, false);
+      document.addEventListener("DOMContentLoaded", modal_init, false);
+    } else {
+      document.getElementById("modal_feedback").attachEvent("onsubmit", checkForm);
+      window.attachEvent("onload", modal_init);
+    }
+  };
+
+  // Create new user 
+const newUser = async () => {
+  const newUser = await fetch(`/api/users/`, { 
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      newUser: newUsername,
+    })
+  })
+  console.log(fetchData)
+  const dataReturn = await fetchData.json();
+  console.log(dataReturn);
+
+  socket.emit('createNewUser', {newUsername});
+  if (newUsername === '') {
+    alert ("Please enter a username")
+  }
